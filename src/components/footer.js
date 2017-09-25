cb1ff2b7636d8df4d61bf716c1d7f9870a3cadbf
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-09-25 14:30:07 -0400

    add header and footer, and react router

---
+++ src/components/footer.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+export default class Footer extends Component {
+  render() {
+    <div className="footer-component" >
+    </div>
+  }
+}
---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+export default class Header extends Component {
+  render() {
+    <div className="header-component" >
+    </div>
+  }
+}
