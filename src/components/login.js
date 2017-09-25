77c30d1098f58b037b54ac8067331c852741bc14
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-09-25 16:32:15 -0400

    add input and change handlers

--- src/components/login.js
+++ src/components/login.js
@@ -4,10 +4,20 @@ import '../styles/App.css';
 export default class Login extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      username : "",
+      password: ""
+    }
+    this.handleTextChange = this.handleTextChange.bind(this);
     this.login = this.login.bind(this);
   }
   login(event){
+    event.preventDefault();
     console.log("login fired")
+  }
+  handleTextChange(event){
+    event.preventDefault();
+    this.setState({[event.target.id]: event.target.value});
   }
   render() {
     return (
@@ -15,6 +25,10 @@ export default class Login extends Component {
         <h1>THIS IS THE LOGIN COMPONENT</h1>
         <div className="loginregister-form-holder">
           <form onSubmit={this.login}>
+            <input onChange={this.handleTextChange} type="text" id="username"
+            placeholder="Username" value={this.state.username}/>
+            <input onChange={this.handleTextChange} type="password" id="password"
+             name="password" value={this.state.password}/>
             <button type="submit">Login</button>
           </form>
         </div>
