e1e39b674427de13766c3efa2f3c7fcce6408460
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-09-25 16:02:42 -0400

    add onclick lisners

--- src/components/loginregistrationpage.js
+++ src/components/loginregistrationpage.js
@@ -6,12 +6,25 @@ import Login from './login.js';
 import Register from './register.js';
 
 export default class LoginRegistrationPage extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        pageswitch: "register"
+      };
+      this.switchpage = this.switchpage.bind(this);
+    }
+  switchpage(x){
+    console.log(x);
+  }
   render() {
     return (
       <div>
       <Header />
         <div className="loginregistrationpage-component" >
         <h1>Login Registration Page</h1>
+          <div className="login-register-switch">
+            <p><a onClick={this.switchpage("login")}>Login</a> | <a onClick={this.switchpage("register")}>Register</a></p>
+          </div>
         </div>
       <Footer />
       </div>
