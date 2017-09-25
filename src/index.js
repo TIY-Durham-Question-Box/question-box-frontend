22538c155dad3129164c8f58f597ddd8f52f0f70
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-09-25 14:38:39 -0400

    add react router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
-import App from './components/App';
+import Home from './components/home.js';
+import LoginRegistrationPage from './components/loginregistrationpage.js';
+import QuestionFrom from './components/questionfrom.js';
+import QuestionPage from './components/questionpage.js';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render( <BrowserRouter>
+    <Switch>
+      <Route path="/index" component={Home}/>
+      <Route path="/login" component={LoginRegistrationPage} />
+      <Route path="/addquestion" component={QuestionFrom} />
+      <Route path="/viewquestion" component={QuestionPage} />
+    </Switch>
+  </BrowserRouter>
+  , document.getElementById('root'));
 registerServiceWorker();
