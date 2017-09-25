8938f745802bdad7d99c8009186f58f742cb9349
Aaron Anderson <aaronlynanderson@gmail.com>
2017-09-25 15:43:13 -0400

    Footer is styled with css grid

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,23 +1,23 @@
 import React, { Component } from 'react';
-import '../styles/App.css';
+import '../styles/footer-styles.css';
 
 export default class Footer extends Component {
   render() {
     return(
       <div className="footer-component" >
-        <div>
+        <div className="footer-component-a">
           Question Box, Ltd.
         </div>
-        <div>
+        <div className="footer-component-b">
           Copyright 2017 under MIT liscense
         </div>
-        <div>
+        <div className="footer-component-c">
           Cookies Disclaimer
         </div>
-        <div>
+        <div className="footer-component-d">
           Terms of Service
         </div>
-        <div>
+        <div className="footer-component-e">
           Contact Us
         </div>
       </div>
