46c59002b0d18fe0df815585b39cf5d6c6ec8453
Aaron Anderson <aaronlynanderson@gmail.com>
2017-09-25 18:06:08 -0400

    Header search form styled

--- src/components/header-components/search-bar.js
+++ src/components/header-components/search-bar.js
@@ -5,10 +5,10 @@ export default class HeaderSearch extends Component {
   render() {
     return(
       <div className="header-search-component" >
-        <form>
-          <label>
-          <input type="text" value="" />
-          <input type="submit" value="Search" />
+        <form className="header-search-form">
+          <label className="header-search-bar">
+          <input className="header-search-input" type="text" value="" />
+          <input className="header-search-button" type="submit" value="Search" />
           </label>
         </form>
       </div>
