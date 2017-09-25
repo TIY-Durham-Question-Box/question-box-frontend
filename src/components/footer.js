21eb55076b16b770026e9ea09e5b6f8cc8828e50
Aaron Anderson <aaronlynanderson@gmail.com>
2017-09-25 15:18:10 -0400

    Establishes basic footer divs

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,6 +5,21 @@ export default class Footer extends Component {
   render() {
     return(
       <div className="footer-component" >
+        <div>
+          Question Box, Ltd.
+        </div>
+        <div>
+          Copyright 2017 under MIT liscense
+        </div>
+        <div>
+          Cookies Disclaimer
+        </div>
+        <div>
+          Terms of Service
+        </div>
+        <div>
+          Contact Us
+        </div>
       </div>
     )
   }
