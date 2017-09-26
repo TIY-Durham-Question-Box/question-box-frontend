28ab7dfa8de3aadfb0cf042ce27f395379aa0cf2
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-09-26 12:25:56 -0400

    add rerouting to search

--- src/components/header-components/search-bar.js
+++ src/components/header-components/search-bar.js
@@ -6,24 +6,32 @@ export default class HeaderSearch extends Component {
   constructor(props){
     super(props);
     this.state = {
-      fireRedirect: false
+      fireRedirect: false,
+      searchbartext: ""
     }
   }
   submitForm = (event) => {
     event.preventDefault();
-    this.setState({ fireRedirect: true })
+    if (this.state.searchbartext){
+      this.setState({ fireRedirect: true, searchbartext: event.target["searchbartext"].value  })
+    }
+  }
+  handleTextChange = (event) => {
+    event.preventDefault();
+    this.setState({[event.target.id]: event.target.value , fireRedirect: false});
   }
   render() {
     return(
       <div className="header-search-component" >
         <form onSubmit={this.submitForm} className="header-search-form">
           <label className="header-search-bar">
-          <input className="header-search-input" type="text" value="" />
+          <input className="header-search-input" onChange={this.handleTextChange}
+          id="searchbartext" placeholder="Search Here..." type="text" value={this.state.searchbartext} />
           <input className="header-search-button" type="submit" value="Search" />
           </label>
         </form>
         {this.state.fireRedirect && (
-          <Redirect to={"/"}/>
+          <Redirect to={`/?=${this.state.searchbartext}`}/>
         )}
       </div>
     )
