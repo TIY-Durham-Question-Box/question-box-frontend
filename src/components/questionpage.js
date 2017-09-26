9ad715f0efe02f51e7eadf91fbb842bb9e9adbca
Aaron Anderson <aaronlynanderson@gmail.com>
2017-09-26 12:00:13 -0400

    Mark up QuestionPage

--- src/components/questionpage.js
+++ src/components/questionpage.js
@@ -1,17 +1,47 @@
 import React, { Component } from 'react';
-import '../styles/App.css';
-import Header from './header.js';
-import Footer from './footer.js';
+import '../styles/question-page.css';
 
 export default class QuestionPage extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      questionId: 1,
+      title: "Question of Randos",
+      language: "JavaScript",
+      tags: ['Question', 'Random'],
+      history: [
+        {
+          answerId: 1,
+          user: "Ted",
+          answer: "Rambo was master of Randos.  Rhonda Rousey was cool for a time, too."
+        }
+      ]
+    }
+  }
+
   render() {
     return (
       <div>
-      <Header />
-        <div className="questionpage-component" >
-        <h1>Question Page</h1>
-        </div>
-      <Footer />
+          <div className="questionpage-component" >
+            <h1>Question Page</h1>
+            <label>
+              Title:
+              <input type="text" value={this.state.title} readOnly/>
+            </label>
+            <label>
+              Language:
+              <input type="text" value={this.state.language} readOnly/>
+            </label>
+            <label>
+              Tags:
+              <input type="text" value={this.state.tags} readOnly/>
+            </label>
+            <label>
+              Answer {this.state.history[0].answerId}:
+              <input type="area" value={this.state.history[0].answer} readOnly/>
+            </label>
+          </div>
       </div>
     )
   }
