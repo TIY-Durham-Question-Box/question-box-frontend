edd49d86f5d6165b7077eb22e820931af2fd18e3
Aaron Anderson <aaronlynanderson@gmail.com>
2017-09-26 15:28:16 -0400

    QuestionPage component has form that can change it's formyness based on whether or not an answer history exists in state

--- src/components/questionpage-components/answers.js
+++ src/components/questionpage-components/answers.js
@@ -6,7 +6,7 @@ export default class Answers extends Component {
   render() {
     console.log(this.props.data);
     return (
-      <div className="answer-component">
+      <div className="answer-component" key={this.props.data.answerId}>
         <div className="answer-attribution">
           <div>Answer {this.props.data.answerId}</div>
           <div>by {this.props.data.user}</div>
--- src/components/questionpage.js
+++ src/components/questionpage.js
@@ -6,6 +6,12 @@ export default class QuestionPage extends Component {
   constructor(){
     super();
 
+    this.setTitle = this.setTitle.bind(this);
+    this.setLanguage = this.setLanguage.bind(this);
+    this.setTags = this.setTags.bind(this);
+    this.setQuestion = this.setQuestion.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
     this.state = {
       questionId: 1,
       title: "Question of Randos",
@@ -13,45 +19,73 @@ export default class QuestionPage extends Component {
       tags: ['Question', 'Random'],
       question: "This is just some place holder text for the question.  This question is about JavaScript, specifically, it's about ECMAscript.  You know, cuz it's kinda cool to talk about random synonyms.",
       history: [
-        {
-          answerId: 1,
-          user: "Ted",
-          answer: "Rambo was master of randos.  Rhonda Rousey was cool for a time, too."
-        },{
-          answerId: 2,
-          user: "Jimbo",
-          answer: "Jimbo was master of jingos.  Meisha Tate was cooler."
-        }
+        // {
+        //   answerId: 1,
+        //   user: "Ted",
+        //   answer: "Rambo was master of randos.  Rhonda Rousey was cool for a time, too."
+        // },{
+        //   answerId: 2,
+        //   user: "Jimbo",
+        //   answer: "Jimbo was master of jingos.  Meisha Tate was cooler."
+        // }
       ]
     }
   }
 
+  setTitle = (e) => {
+    this.setState({title:e.target.value});
+  }
+  setLanguage = (e) => {
+    this.setState({language:e.target.value});
+  }
+  setTags = (e) => {
+    this.setTags({tags:e.target.value});
+  }
+  setQuestion = (e) => {
+    this.setState({question:e.target.value});
+  }
+  handleSubmit = (e) => {
+    e.prevendDefault();
+    console.log(this.state);
+  }
+
+
   render() {
     let allAnswers = this.state.history;
     let answers = allAnswers.map((answers)=>{
-      return(<Answers data={answers}/>)
+      return(<Answers key={answers.answerId} data={answers} />)
     });
+    let locked = false;
+    let editButton = null;
+    if (this.state.history.length > 0){
+      locked = true;
+    } else {
+      editButton = <input type="Submit" value="Submit"/>
+    }
 
     return (
       <div>
           <div className="questionpage-component" >
+            <form onSubmit={this.handleSubmit}>
             <h1>Question Page</h1>
             <p>
               Title:
-              <input type="text" value={this.state.title} readOnly/>
+              <input type="text" onChange={this.setTitle} value={this.state.title} readOnly={locked} />
             </p>
             <p>
               Language:
-              <input type="text" value={this.state.language} readOnly/>
+              <input type="text" onChange={this.setLanguage} value={this.state.language} readOnly={locked} />
             </p>
             <p>
               Tags:
-              <input type="text" value={this.state.tags} readOnly/>
+              <input type="text" onChange={this.setTags} value={this.state.tags} readOnly={locked} />
             </p>
             <p className="active-question-textarea">
               Question:
-              <textarea value={this.state.question} readOnly />
+              <textarea onChange={this.setQuestion} value={this.state.question} readOnly={locked} />
             </p>
+              {editButton}
+            </form>
               {answers}
           </div>
       </div>
