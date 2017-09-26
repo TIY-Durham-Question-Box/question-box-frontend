84449ea6a68f5131b95aba90d7eee64299e9aed0
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-09-26 12:13:25 -0400

    add redirect to header

--- src/components/header-components/search-bar.js
+++ src/components/header-components/search-bar.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
 import '../../styles/header-styles.css';
+import { Redirect } from 'react-router-dom';
 
 export default class HeaderSearch extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      fireRedirect: false
+    }
+  }
+  submitForm = (event) => {
+    event.preventDefault();
+    this.setState({ fireRedirect: true })
+  }
   render() {
     return(
       <div className="header-search-component" >
-        <form className="header-search-form">
+        <form onSubmit={this.submitForm} className="header-search-form">
           <label className="header-search-bar">
           <input className="header-search-input" type="text" value="" />
           <input className="header-search-button" type="submit" value="Search" />
           </label>
         </form>
+        {this.state.fireRedirect && (
+          <Redirect to={"/"}/>
+        )}
       </div>
     )
   }
